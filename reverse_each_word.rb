35d008c95fc10f0ce63b2e46ed88f3ccdc9bf68e
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 19:37:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   array = []
   reverse = []
   array = string.split(" ")
-  array.each {|string| reverse << string.reverse_each}
+  array.each {|string| string.split("")}
   return reverse
 end
